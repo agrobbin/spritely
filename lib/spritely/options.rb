6dfc6feeaef3b7e04150f6657e64a123d542df5a
Alex Robbin <alex.robbin@meyouhealth.com>
2014-05-19 23:58:06 -0400

    correctly parse Sass::Script objects when passed in as options

--- lib/spritely/options.rb
+++ lib/spritely/options.rb
@@ -5,9 +5,9 @@ module Spritely
     attr_reader :options
 
     def initialize(hash)
-      @options = hash.inject({}) do |h, (key, value)|
+      @options = hash.inject({}) do |h, (key, sass_object)|
         split_key = key.split('_')
-        option = {split_key.pop.to_sym => value}
+        option = {split_key.pop.to_sym => sass_object.value}
         h.deep_merge!(split_key.join('-') => option)
       end
     end
--- spec/spritely/options_spec.rb
+++ spec/spritely/options_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe Spritely::Options do
-  let(:hash) { {'some_new_image_x' => 123, 'some_new_image_y' => 456, 'another_image_repeat' => true} }
+  let(:hash) { {
+    'some_new_image_x' => Sass::Script::Number.new(123),
+    'some_new_image_y' => Sass::Script::Number.new(456),
+    'another_image_repeat' => Sass::Script::Bool.new(true),
+    'yet_another_image_repeat' => Sass::Script::Bool.new(false)
+  } }
 
   subject(:options) { Spritely::Options.new(hash) }
 
   its(['some-new-image']) { should eq({x: 123, y: 456}) }
   its(['another-image']) { should eq({repeat: true}) }
+  its(['yet-another-image']) { should eq({repeat: false}) }
 
   describe '#[]' do
     it 'should fall back to an empty hash' do
