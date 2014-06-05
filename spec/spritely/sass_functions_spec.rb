388a6baee59f5afe837241c44fc05f69d9ab966e
Alex Robbin <alex.robbin@meyouhealth.com>
2014-06-05 19:14:15 -0400

    (try to) simplify SassFunctions tests

--- spec/spritely/sass_functions_spec.rb
+++ spec/spritely/sass_functions_spec.rb
@@ -6,47 +6,41 @@ describe Spritely::SassFunctions do
     def files; []; end
   end
 
-  let(:sprite_map) { SpriteMapDouble.new }
+  module AssetUrlModule
+    def asset_url(path)
+      Sass::Script::String.new("url(#{path})")
+    end
+  end
 
-  before { allow(Spritely::SpriteMap).to receive(:create).and_return(sprite_map) }
+  let(:sprite_map) { SpriteMapDouble.new }
+  let(:image) { double(left: 10, top: 12, width: 25, height: 50) }
+
+  before do
+    allow(Spritely::SpriteMap).to receive(:create).and_return(sprite_map)
+    allow(sprite_map).to receive(:find).with('bar').and_return(image)
+  end
+
+  before(:all) { ::Sass::Script::Functions.send(:include, AssetUrlModule) }
+  after(:all) { ::Sass::Script::Functions.send(:undef_method, :asset_url) }
 
   shared_examples "a sprite function that checks image existence" do
-    before { allow(sprite_map).to receive(:find).with('bar').and_return(image) }
+    let(:image) { nil }
 
-    context 'the image does not exist' do
-      let(:image) { nil }
-
-      it 'should raise a nice exception' do
-        expect { subject }.to raise_error(Sass::SyntaxError, "No image 'bar' found in sprite map 'test'.")
-      end
+    it 'should raise a nice exception' do
+      expect { subject }.to raise_error(Sass::SyntaxError, "No image 'bar' found in sprite map 'test'.")
     end
   end
 
   describe '#spritely_url' do
-    before do
-      asset_url_module = Module.new do
-        def asset_url(path)
-          Sass::Script::String.new("url(#{path})")
-        end
-      end
-      ::Sass::Script::Functions.send(:include, asset_url_module)
-    end
+    subject { evaluate("spritely-url(spritely-map('test/*.png'))") }
 
-    after do
-      ::Sass::Script::Functions.send(:undef_method, :asset_url)
-    end
-
-    it "should use Rails' built-in asset_url function" do
-      expect(evaluate("spritely-url(spritely-map('test/*.png'))")).to eq('url(sprites/test.png)')
-    end
+    it { should eq('url(sprites/test.png)') }
   end
 
   describe '#spritely_position' do
-    let(:image) { double(left: 10, top: 12) }
-
     subject { evaluate("spritely-position(spritely-map('test/*.png'), 'bar')") }
 
-    include_examples "a sprite function that checks image existence"
+    it_should_behave_like "a sprite function that checks image existence"
 
     it { should eq('-10px -12px') }
 
@@ -58,21 +52,17 @@ describe Spritely::SassFunctions do
   end
 
   describe '#spritely_width' do
-    let(:image) { double(width: 25) }
-
     subject { evaluate("spritely-width(spritely-map('test/*.png'), 'bar')") }
 
-    include_examples "a sprite function that checks image existence"
+    it_should_behave_like "a sprite function that checks image existence"
 
     it { should eq('25px') }
   end
 
   describe '#spritely_height' do
-    let(:image) { double(height: 50) }
-
     subject { evaluate("spritely-height(spritely-map('test/*.png'), 'bar')") }
 
-    include_examples "a sprite function that checks image existence"
+    it_should_behave_like "a sprite function that checks image existence"
 
     it { should eq('50px') }
   end
