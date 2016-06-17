97eb945545e312adc56b3c8a0cd10bfa4e55b780
Alex Robbin <agrobbin@gmail.com>
2016-06-16 22:26:58 -0400

    make sure we're correctly generating sprite maps that have no custom settings

--- spec/integration/precompilation_spec.rb
+++ spec/integration/precompilation_spec.rb
@@ -15,6 +15,13 @@ describe 'Precompilation', :integration do
     expect(sprite_files.length).to eq(1)
   end
 
+  it 'should compile a sprite that has no directives' do
+    runner %~Rails.application.assets["sprites/empty.png"].write_to("compiled-sprite.png")~
+    compiled_sprite = ChunkyPNG::Image.from_file('compiled-sprite.png')
+    correct_sprite = ChunkyPNG::Image.from_file(File.join(__dir__, '..', 'fixtures', 'correct-empty-sprite.png'))
+    expect(compiled_sprite).to eq(correct_sprite)
+  end
+
   it 'should compile all of the assets necessary when sprites have been pre-generated' do
     render_asset('sprites.css')
     compile_assets
